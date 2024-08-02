b95d1d17e2189a449f33a55580bd17721a9d588d
Utkarsh Tiwari <uktiwari023@gmail.com>
2024-08-02 16:13:01 +0530

    add  more avatar and bug fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,10 @@ module.exports = {
   content: ["./views/*.ejs"],
   safelist: [
     'text-green-500',
-    'text-red-500'
+    'text-red-500',
+    'border-red-500',
+    'border-zinc-200',
+    'border-4'
   ],
   theme: {
     extend: {},
